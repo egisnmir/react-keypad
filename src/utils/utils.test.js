d205406cdecc09db350597da4c460554d9497bfc
Egidijus <egisnmir@gmail.com>
2021-04-23 17:08:06 +0300

    test refactor

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -3,7 +3,7 @@ import { maskNumber } from './utils'
 //TODO
 describe('Keypad component tests', () => {
   it('tests if the maskNumber function works correctly', () => {
-    const maskedValue = maskNumber(1243);
+    const maskedValue = maskNumber(1234);
 
     expect(maskedValue).toBe('***4');
   });
